98911653fb108d69953ec892efb2c8d98814240f
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-06-14 12:02:29 +0900

    update Category, Comment repository
    save 기능 추가

--- src/main/java/com/java/web/solutionhub/board/repository/CategoryRepository.java
+++ src/main/java/com/java/web/solutionhub/board/repository/CategoryRepository.java
@@ -1,5 +1,20 @@
 package com.java.web.solutionhub.board.repository;
 
-public class CategoryRepository {
+import javax.persistence.EntityManager;
 
+import org.springframework.stereotype.Repository;
+
+import com.java.web.solutionhub.board.domain.Category;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class CategoryRepository {
+	
+	private final EntityManager em;
+	
+	public void save(Category category) {
+		em.persist(category);
+	}
 }

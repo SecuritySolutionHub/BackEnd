be8858e80740670307d07ac814cc9a75dd9834ab
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-07-23 00:33:28 +0900

    Update ReviewDto.java
    ReviewDto 생성자 설정 추가

--- src/main/java/com/java/web/solutionhub/board/domain/ReviewDto.java
+++ src/main/java/com/java/web/solutionhub/board/domain/ReviewDto.java
@@ -2,11 +2,13 @@ package com.java.web.solutionhub.board.domain;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class ReviewDto {
 	
 	private Long id;
